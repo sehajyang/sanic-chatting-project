855dfe57f5053929b61b3e6a0e54d205e29a1388
sehajyang <sehajyang@gmail.com>
2019-04-11 15:02:52 +0900

    Mod pg_get_set 몇가지 함수 추가 및 requirement 업데이트

--- pg_handle/pg_get_set.py
+++ pg_handle/pg_get_set.py
@@ -8,5 +8,17 @@ dotenv.load_dotenv()
 
 async def get_pg_connection():
     return await asyncpg.connect(user=os.environ['PG_USER'], password=os.environ['PG_PASSWORD'],
-                             database=os.environ['PG_DB'], host=os.environ['PG_HOST'])
+                                 database=os.environ['PG_DB'], host=os.environ['PG_HOST'])
 
+
+async def get_user_password_by_id(id):
+    return await get_pg_connection().fetch(
+        'SELECT * FROM users WHERE id = $id', id
+        )
+
+
+# TODO: 파라미터 이렇게 넘기지 말 것
+async def set_user_data(id, name ,password):
+    return await get_pg_connection().execute('''
+        INSERT INTO users VALUES ($id), ($password), ($name)
+        ''', id, password, name)
